4dfa4ce454e013a6301fa062161f01d553eb69d1
AlienTab1 <ales.tabarka@email.cz>
2025-05-06 00:20:06 +0200

    trixie conf updated of kernel version 6.12.25

--- tests/sch_test_contextswitch.sh
+++ tests/sch_test_contextswitch.sh
@@ -9,22 +9,9 @@ echo "Current CPU threads: $threads"
 echo "Test duration: $duration seconds"
 echo "=== Scheduler Context Switching Test ==="
 
-run_vmstat_test() {
-    echo
-    echo ">>> [1/2] Measuring system-wide context switches with vmstat"
-    echo "Starting CPU load using stress-ng with $threads threads..."
-    stress-ng --cpu "$threads" --timeout "$duration" --quiet &
-    stress_pid=$!
-    sleep 2
-    echo
-    echo "--- vmstat output (context switch column = 'cs') ---"
-    vmstat 1 "$duration"
-    wait $stress_pid
-}
-
 run_pidstat_test() {
     echo
-    echo ">>> [2/2] Measuring per-thread context switches with pidstat -w -t"
+    echo ">>> Measuring per-thread context switches with pidstat -w -t"
     echo "Starting stress-ng again for detailed tracking..."
     stress-ng --cpu "$threads" --timeout "$(($duration+3))" --quiet &
     stress_pid=$!
@@ -37,9 +24,7 @@ run_pidstat_test() {
 
 # === Run both parts ===
 #run_vmstat_test
-echo
-echo "Sleeping for 5 seconds before running pidstat..."
-sleep 5
+
 run_pidstat_test
 
 echo
